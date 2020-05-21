7f8710d7080ee2de49178dbd8c00ddf43c35a948
J Plourde <jmichel.plourde@gmail.com>
2020-05-21 10:35:09 -0400

    Correction des fautes

--- script.py
+++ script.py
@@ -5,17 +5,17 @@ def mood():
     while True:
         res = input("Ca va? \n[a] Ca va (Good) \n[b] Calme (Calm) \n[c] Content/Contente (happy) \n[d] Fatigue/Fatiguee (Tired) \n[e] inquiet/inquiete (worried) \n[f] stresse/stressee (stressed) \n[g] triste (sad) \n>")
         if res == "a":
-            return "ca va"
+            return "ça va"
         elif res == "b":
             return "calme"
         elif res == "c":
             return "content/contente"
         elif res == "d":
-            return "fatigue/fatiguee"
+            return "fatigué/fatiguée"
         elif res == "e":
-            return "inquiet/inquiete"
+            return "inquiet/inquiète"
         elif res == "f":
-            return "stresse/stressee"
+            return "stressé/stressée"
         elif res == "g":
             return "triste"
         else:
@@ -23,8 +23,8 @@ def mood():
 
 def greeting():
     print("Bonjour")
-    user_name = input("Je m'appelle Cyberbarbie, et vous? ")
-    print(f"Enchantee, {user_name}!")
+    user_name = input("Je m'appelle Cyberbarbie et vous? ")
+    print(f"Enchantée, {user_name}!")
     feeling = mood()
     print(f"Apparently you're feeling {feeling}")
 
